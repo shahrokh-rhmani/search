af051cee10e88823b0ac6deba9089237b143ef0e
Ali <shahrokh.r1379@gmail.com>
2022-02-09 20:52:02 +0330

    HomePage & SearchResultView

--- search/views.py
+++ search/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import City
 
-# Create your views here.
+
+
+class HomePage(TemplateView):
+    template_name = 'home.html'
+
+
+
+
+class SearchResultView(ListView):
+    model = City
+    template_name = 'search.html'   
