3b47cc124413bce7b7f68eafdf43dbac34e99c9a
Ali <shahrokh.r1379@gmail.com>
2022-02-12 21:02:13 +0330

    complete get_queryset

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import City
+from django.db.models import Q
 
 
 
@@ -14,3 +15,10 @@ class SearchResultView(ListView):
     model = City
     context_object_name = 'search_list'
     template_name = 'search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        search_list = City.objects.filter(
+            Q(name__icontains=query) | Q(country__icontains=query)
+        )
+        return search_list
